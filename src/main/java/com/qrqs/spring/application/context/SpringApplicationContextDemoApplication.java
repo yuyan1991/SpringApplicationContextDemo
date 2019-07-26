0774f3a1ab46a73a4b27cce74531fbf83187f9d1
yuyan <yuyan1991@gmail.com>
2019-07-26 12:13:45 +0800

    construct environment for spring application context demo

---
+++ src/main/java/com/qrqs/spring/application/context/SpringApplicationContextDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qrqs.spring.application.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationContextDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationContextDemoApplication.class, args);
+	}
+
+}
